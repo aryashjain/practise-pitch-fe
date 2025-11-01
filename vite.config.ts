f578bf319f039f04575b22b8bd0a59413773f01c
Aryash Jain <aryash.enu@gmail.com>
2025-11-02 00:59:48 +0530

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins: [react()],
-server: {
-proxy: {
-'/api': {
-target: 'https://practise-pitch-be.onrender.com',
-changeOrigin: true,
-secure: false,
-},
-},
-},
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // needed for Render / external access
+    port: Number(process.env.PORT) || 5173,
+
+    proxy: {
+      '/api': {
+        target: 'https://practise-pitch-be.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
